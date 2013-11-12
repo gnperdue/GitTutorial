02633a760360e254df16a55b57f7ec0b44eec6c5
Gabriel Perdue <perdue@fnal.gov>
2013-11-12 08:02:35 -0600

    Don't need to compile chapters individually unless we want to.

--- GitInLittleBits/compile.sh
+++ GitInLittleBits/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-asciidoc -b html5 -a data-uri -a toc2 lesson001.adoc 
-asciidoc -b html5 -a data-uri -a toc2 lesson002.adoc 
+# Don't think we need to compile individual docs...
+# asciidoc -b html5 -a data-uri -a toc2 lesson001.adoc 
+# asciidoc -b html5 -a data-uri -a toc2 lesson002.adoc 
 asciidoc -b html5 -a data-uri -a toc2 GitInLittleBits.adoc 
 
 # a2x --fop -f pdf GitInLittleBits.adoc
